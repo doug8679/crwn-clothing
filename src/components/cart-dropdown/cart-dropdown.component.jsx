32c046357933af4ce4cf6d59da90ac6ecb6d7d45
db250583 <Doug.Braxton@ncr.com>
2020-07-02 16:42:12 -0400

    Build out checkout page and checkout item
    Added functionality and styling ot checkout page.
    Added checkout item component for listing items in cart during checkout.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,12 +5,13 @@ import { withRouter } from 'react-router-dom';
 
 import CustomButtom from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -22,7 +23,10 @@ const CartDropdown = ({ cartItems, history }) => (
             <span className='empty-message'>Your cart is empty</span>
         }
         </div>
-        <CustomButtom onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButtom>
+        <CustomButtom onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden());
+        }}>GO TO CHECKOUT</CustomButtom>
     </div>
 );
 
