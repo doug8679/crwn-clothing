556559b251d5fd55ff2c4df61e50fba2d306d1eb
db250583 <Doug.Braxton@ncr.com>
2020-07-04 13:41:50 -0400

    Re-sync with instructor code-base
    Missed something in one of the lecture and my code got out of sync.  Had
    to re-apply some fies from the lesson-20 repository snapshot.

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 import './cart-item.styles.scss';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     <div className='cart-item'>
-        <img src={imageUrl} alt={name} />
+        <img src={imageUrl} alt='item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>{quantity} x ${price}</span>
@@ -13,4 +12,4 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
     </div>
 );
 
-export default connect()(CartItem);
+export default CartItem;
